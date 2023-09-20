074d5bb97ca00d55f24d6d6d46fc118cd560b060
Nameless-itf23 <ohutoncat@gmail.com>
2023-09-20 17:01:08 +0900

    change main format

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 from insightface.app import FaceAnalysis
 import db
 import time
+import os
 
 app = FaceAnalysis()
 app.prepare(ctx_id=1, det_size=(640, 640))
@@ -10,19 +11,25 @@ app.prepare(ctx_id=1, det_size=(640, 640))
 def cos_sim(feat1, feat2):
     return np.dot(feat1, feat2) / (np.linalg.norm(feat1) * np.linalg.norm(feat2))
 
-face_num = 2  # 登録した顔の数
-faces = []
-for i in range(face_num):
-    img = cv2.imread(f'faces/{i}.jpg')
-    tmp = app.get(img)
-    vec = tmp[0].embedding
-    faces.append(vec)
+faces_dir = './faces'
+folders = {}
+with os.scandir(faces_dir) as fs:
+    for entry in fs:
+        if entry.is_dir():
+            folders[entry.name] = []
+            with os.scandir(faces_dir+'/'+entry.name) as folder:
+                for face in folder:
+                    img = cv2.imread(faces_dir+'/'+entry.name+'/'+face.name)
+                    tmp = app.get(img)
+                    vec = tmp[0].embedding
+                    folders[entry.name].append(vec)
 
 def match(vec):
     ans = [0, -1]
-    for i in range(len(faces)):
-        if cos_sim(vec, faces[i]) > ans[1]:
-            ans = [i, cos_sim(vec, faces[i])]
+    for name, faces in folders.items():
+        for face in faces:
+            if cos_sim(vec, face) > ans[1]:
+                ans = [name, cos_sim(vec, face)]
     if ans[1] > 0.5:
         return ans[0]
     else:

3bb4765fec6c3fa2b9f2c7edb591224f595ac7fb
Nameless-itf23 <ohutoncat@gmail.com>
2023-09-20 16:44:34 +0900

    change db format

--- db.py
+++ db.py
@@ -1,2 +1,24 @@
 import json
 import time
+
+def write(user_id: str, date: int):
+    with open('db.json') as f:
+        db = json.load(f)
+    if user_id not in db.keys():
+        db[user_id] = []
+    db[user_id].append(date)
+    with open('db.json', 'w') as f:
+        json.dump(db, f, indent=2)
+
+def trim(hour):  # 直近hour時間のデータのみにtrim
+    with open('db.json') as f:
+        db = json.load(f)
+    now = time.time()
+    for key, value in db.items():
+        ans = []
+        for i in value:
+            if now - i < hour * 3600:
+                ans.append(i)
+        db[key] = ans
+    with open('db.json', 'w') as f:
+        json.dump(db, f, indent=2)

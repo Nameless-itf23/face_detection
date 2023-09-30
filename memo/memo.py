8c7c5a93768c263ddcbfe4f49cdc0106c2535704
Nameless-itf23 <ohutoncat@gmail.com>
2023-09-30 18:18:17 +0900

    .

--- memo/memo.py
+++
@@ -1,6 +0,0 @@
-import time
-import datetime
-import zoneinfo
-date = int(time.time())
-d = datetime.datetime.fromtimestamp(date, datetime.timezone.utc).astimezone(zoneinfo.ZoneInfo("Asia/Tokyo"))
-print(str(d)[:10])
